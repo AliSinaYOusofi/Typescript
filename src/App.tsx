dcd3d310f11dd93756d5238596c9537a878a4259
artLinus <tinayousofiali@gmail.com>
2022-10-07 20:30:39 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import { useLayoutEffect } from "react";
 import "./styles.css";
 
 export default function App() {
+  let employees: Object = {
+    name: "ha",
+    last: "bib",
+    place: "German",
+  };
+
+  let vacantObject: {}; // because this is fucking vacant
+  let arrayOfNames: string[] = ["Ali Sina", "Yousofi", "catch and halt"];
+  let infer = arrayOfNames[0]; // the type infered
+
+  // array of mixed type
+  let mixedArrayOfTypes: (string | Number | Object)[] = [
+    { one: "1" },
+    "Ali",
+    444,
+  ];
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> {employees.last + employees.name} </h1>
+      <p> {arrayOfNames[0]} </p>
+      <p> {typeof infer}</p>
+      <ul>
+        <li> {mixedArrayOfTypes[0].one} </li>
+      </ul>
     </div>
   );
 }
