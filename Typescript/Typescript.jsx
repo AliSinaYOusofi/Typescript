a49f459fa0855ea827c0b5f5a05ed9b304200a57
artLinus <tinayousofiali@gmail.com>
2022-10-08 20:18:37 +0000

    Update Typescript.jsx

--- Typescript/Typescript.jsx
+++ Typescript/Typescript.jsx
@@ -23,4 +23,20 @@ function nothing() : void {
     let nothing : void = undefined; // only undefined to void type
     console.log("nothing functions")
 }
-    
+
+function throwOnlyError () : never {
+    throw new Error("never only throws error");
+}
+
+function indefiniteLoop() : never {
+    while (true) console.log("error");
+}
+
+function makeMeValid(a : number | string, b: number | string, ...opt: number[]) : boolean {
+   if (typeof a === "string") return true;
+   else if (typeof a === "number") return false;
+    // making the function valid just in case
+    return makingValid();
+}
+
+function makingValid() : never { throw new Error("Made it Valid")};
