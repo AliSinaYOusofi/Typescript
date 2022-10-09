b0b2790a2c1a3e03c75547d37499a98c5dcd2e0a
artLinus <tinayousofiali@gmail.com>
2022-10-09 18:37:55 +0000

    Update Classes.ts

--- Typescript/Classes.ts
+++ Typescript/Classes.ts
@@ -1,4 +1,5 @@
 class Students {
+  
   private student_name: string;
   private student_age: string;
   private student_full_name: string;
@@ -25,5 +26,15 @@ class Students {
     this.student_name = student_name;
     this.student_age = student_age;
     this.student_full_name = student_full_name;
+  }
+
+  public setName(name: string): void {
+    this.student_name = name;
+  }
+  public setStudentAge(age: number): void {
+    this.student_age = age;
+  }
+  public setStudentFullName(full_name: string = "na") {
+    this.student_full_name = full_name;
   }
 }
