149dac2b9af0ed5c120dcd9cb8e7a11b82c0d814
artLinus <tinayousofiali@gmail.com>
2022-10-12 18:22:44 +0000

    Create Casting.ts

---
+++ Typescript/Casting.ts
@@ -0,0 +1,74 @@
+interface Employee {
+  name: string;
+  last: string;
+}
+
+interface Work {
+  grid: boolean;
+  lost: string;
+}
+
+type ofBoth = Employee | Work;
+
+let dict: ofBoth = {
+  name: "ALi",
+  last: "Last",
+  grid: true,
+  lost: "yes",
+};
+
+let employee: Employee = {
+  name: "Fuck",
+  last: "No",
+};
+
+let worker: Work = {
+  grid: true,
+  lost: "fuck",
+};
+
+let BothWorker: Work & Employee = {
+  name: "fuc",
+  lost: "no",
+  grid: true,
+  last: "lst",
+};
+
+let lengthOf: string = "Helloworld";
+let findLength: number = (lengthOf as string).length;
+let lengthAsNumber: string = (lengthOf as string).split("")[0].toString();
+console.log(findLength, lengthAsNumber);
+
+// not the same as other languages
+
+class Class {
+  private className: string;
+
+  constructor(private classMainName: string) {
+    this.className = classMainName;
+  }
+
+  get getClassName(): string {
+    return this.className;
+  }
+}
+
+class Hovering {
+  private isHovered: boolean;
+
+  constructor(private isHovering: boolean) {
+    this.isHovered = isHovering;
+  }
+
+  get getHovering(): boolean {
+    return this.isHovered;
+  }
+  moreData(data: string = "not passed"): string {
+    return "more Data Returned" + data;
+  }
+}
+
+let Classy = new Class("main Class Name");
+let Hover = new Hovering(true);
+
+console.log((Classy as unknown as Hovering).moreData("data")); // what the fuck
